59a513d7265ec04ec73796e5b01f8b10707457eb
CunningBird <68229355+CunningBird@users.noreply.github.com>
2021-10-15 06:42:46 +0300

    initial commit

---
+++ admin/README.md
@@ -0,0 +1,1 @@
+# Gym Team app - Admin Service
---
+++ admin/src/test/java/com/gymteam/backend/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymteam.backend.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
