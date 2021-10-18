c3843cb35c7caa53e0c8e7da79e04827d1bec635
CunningBird <68229355+CunningBird@users.noreply.github.com>
2021-10-18 22:29:33 +0300

    Implement open-feign

--- admin/src/main/java/com/gymteam/backend/admin/AdminApplication.java
+++ admin/src/main/java/com/gymteam/backend/admin/AdminApplication.java
@@ -3,6 +3,7 @@ package com.gymteam.backend.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 @RestController
 public class AdminApplication {
 
