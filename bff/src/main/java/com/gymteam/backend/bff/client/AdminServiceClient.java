6b947bd9b80f0d64bc09e299e62035bb5d6a567a
Alex <a>
2021-10-20 20:57:33 +0300

    Create Admin: schema,controller,client,entity

--- bff/src/main/java/com/gymteam/backend/bff/client/AdminServiceClient.java
+++ bff/src/main/java/com/gymteam/backend/bff/client/AdminServiceClient.java
@@ -1,8 +1,14 @@
 package com.gymteam.backend.bff.client;
 
+import com.gymteam.backend.bff.dto.Admin;
+
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.UUID;
 
 @FeignClient("admin-service")
 public interface AdminServiceClient {
@@ -10,4 +16,12 @@ public interface AdminServiceClient {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     String hello();
 
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    Admin createAdmin(@RequestBody Admin admin);
+
+    @RequestMapping(value = "/", method = RequestMethod.PUT)
+    Admin updateAdmin(@RequestBody Admin admin);
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    Admin getAdmin(@PathVariable UUID id);
 }
