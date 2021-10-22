fdc827350a759457d3498f75a0c407a01690cfc7
Alex <a>
2021-10-23 00:02:27 +0300

    Add DB migration which create 10 users in user_service

---
+++ user/src/main/resources/db/migration/V2__createUsers.sql
@@ -0,0 +1,30 @@
+INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
+VALUES('n4i8x9a@n4i8x9a.ru','ReFf2281488','Alex','Nix',gen_random_uuid());
+
+INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
+VALUES('abobus@abo.ba','FdSaQw','ABOBU','SSS',gen_random_uuid());
+
+INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
+VALUES('felix433@gmail.com','QwErTy','Felix','Zxx',gen_random_uuid());
+
+INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
+VALUES('ivanov342@yandex.ru','ZxSaQwE','Ivanov','Ivan',gen_random_uuid());
+
+INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
+VALUES('tom5432@outlook.com','QwEr3Q','Tom','Jonson',gen_random_uuid());
+
+INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
+VALUES('anon228@mail.ru','rhgthtrherhg','Anon','Anon',gen_random_uuid());
+
+INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
+VALUES('zzz21@yandex.ru','dddefwgwrgw','Alexander','Alexandrov',gen_random_uuid());
+
+INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
+VALUES('zzz123@gmail.com','Egwgwgwrg','Egor','L',gen_random_uuid());
+
+INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
+VALUES('zz12@vk.com','DwEqW','Mike','R',gen_random_uuid());
+
+INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
+VALUES('ds@test.com','Fewgghrg','DDD','DDD',gen_random_uuid());
+
