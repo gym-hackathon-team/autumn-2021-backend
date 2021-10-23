fc68ad2f58a017b13cb5f6b48999aa5a2b9253a3
Alex <a>
2021-10-23 06:22:14 +0300

    return userId in auth response

--- auth/src/main/java/com/gymteam/backend/auth/dto/bff/TokenDto.java
+++ auth/src/main/java/com/gymteam/backend/auth/dto/bff/TokenDto.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class TokenDto {
-
+    private UUID userId;
     private String token;
 }
