4d36e6dd9080fe19dd5a0c3305400e6d7af2fbeb
Alex <a>
2021-10-23 19:57:02 +0300

    add test data for account,organization, user service

--- user/src/main/resources/db/migration/V2__createUsers.sql
+++ user/src/main/resources/db/migration/V2__createUsers.sql
@@ -1,30 +1,8 @@
 INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
-VALUES('n4i8x9a@n4i8x9a.ru','ReFf2281488','Alex','Nix',gen_random_uuid());
+VALUES('n4i8x9a@n4i8x9a.ru','ReFf2281488','Alex','Nix','689198ba-3418-11ec-8d3d-0242ac130003');
 
 INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
-VALUES('abobus@abo.ba','FdSaQw','ABOBU','SSS',gen_random_uuid());
+VALUES('abobus@abo.ba','FdSaQw','ABOBU','SSS','955a2006-3418-11ec-8d3d-0242ac130003');
 
-INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
-VALUES('felix433@gmail.com','QwErTy','Felix','Zxx',gen_random_uuid());
 
-INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
-VALUES('ivanov342@yandex.ru','ZxSaQwE','Ivanov','Ivan',gen_random_uuid());
-
-INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
-VALUES('tom5432@outlook.com','QwEr3Q','Tom','Jonson',gen_random_uuid());
-
-INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
-VALUES('anon228@mail.ru','rhgthtrherhg','Anon','Anon',gen_random_uuid());
-
-INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
-VALUES('zzz21@yandex.ru','dddefwgwrgw','Alexander','Alexandrov',gen_random_uuid());
-
-INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
-VALUES('zzz123@gmail.com','Egwgwgwrg','Egor','L',gen_random_uuid());
-
-INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
-VALUES('zz12@vk.com','DwEqW','Mike','R',gen_random_uuid());
-
-INSERT INTO USERS (EMAIL, PASSWORD,FIRST_NAME,LAST_NAME,ID)
-VALUES('ds@test.com','Fewgghrg','DDD','DDD',gen_random_uuid());
 
