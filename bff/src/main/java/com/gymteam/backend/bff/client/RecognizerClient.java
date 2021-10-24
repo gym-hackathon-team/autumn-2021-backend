5909525be552d4adbcb3a14038c9e7c57bb53f4a
CunningBird <68229355+CunningBird@users.noreply.github.com>
2021-10-24 17:15:25 +0300

    Fix request

--- bff/src/main/java/com/gymteam/backend/bff/client/RecognizerClient.java
+++ bff/src/main/java/com/gymteam/backend/bff/client/RecognizerClient.java
@@ -2,15 +2,14 @@ package com.gymteam.backend.bff.client;
 
 import com.gymteam.backend.bff.dto.recognize.RecognizedVoiceDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient(name = "recognizer-service", url = "http://recognizer:8088/")
-//@FeignClient(name = "recognizer-service")
+@FeignClient(name = "recognizer-service")
 public interface RecognizerClient {
 
     @RequestMapping(value = "/recognize", method = RequestMethod.POST)
-    RecognizedVoiceDto analyzeVoice(@RequestBody MultipartFile upload_file); // TODO File variable type
+    RecognizedVoiceDto analyzeVoice(@RequestParam("upload_file") MultipartFile upload_file); // TODO File variable type
 }
--- bff/src/main/java/com/gymteam/backend/bff/service/impl/VoiceServiceImpl.java
+++ bff/src/main/java/com/gymteam/backend/bff/service/impl/VoiceServiceImpl.java
@@ -14,6 +14,7 @@ import com.gymteam.backend.bff.service.interfaces.VoiceService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.net.HttpURLConnection;
