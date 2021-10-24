0b91b746701a8674b932dbe548fbb7b687ed2d02
CunningBird <68229355+CunningBird@users.noreply.github.com>
2021-10-24 16:03:13 +0300

    Connect to Recognize

--- bff/src/main/java/com/gymteam/backend/bff/service/impl/VoiceServiceImpl.java
+++ bff/src/main/java/com/gymteam/backend/bff/service/impl/VoiceServiceImpl.java
@@ -16,6 +16,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.Locale;
 import java.util.Objects;
 
@@ -29,10 +33,7 @@ public class VoiceServiceImpl implements VoiceService {
 
     @Override
     public VoiceCommandResponse authorizeVoiceCommand(MultipartFile multipart) throws VoiceNotMatchingException, UnknownVoiceCommandException {
-        //RecognizedVoiceDto recognized = recognizerClient.analyzeVoice(multipart); TODO enable this
-        RecognizedVoiceDto recognized = new RecognizedVoiceDto();
-        recognized.setVoiceId("1.7580954596257448");
-        recognized.setWords("Пх, окей перевод пользователю пожалуйста. Кхм");
+        RecognizedVoiceDto recognized = recognizerClient.analyzeVoice(multipart);
 
         Authorized authorized = (Authorized) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         UserDto user = userClient.getUser(authorized.getId()); // Пользователь точно будет, ибо иначе авторизация не пустит сюда
@@ -60,13 +61,8 @@ public class VoiceServiceImpl implements VoiceService {
     }
 
     @Override
-    public void registerUserVoice(MultipartFile multipartFile) throws VoiceNotRegisteredException {
-        //RecognizedVoiceDto recognized = recognizerClient.analyzeVoice(multipartFile); TODO enable this
-
-        RecognizedVoiceDto recognized = new RecognizedVoiceDto();
-        recognized.setWords("окей абоба перевод пользователю андрей");
-
-        recognized.setVoiceId("1.7580954596257448");
+    public void registerUserVoice(MultipartFile multipartFile) throws VoiceNotRegisteredException, MalformedURLException, ProtocolException {
+        RecognizedVoiceDto recognized = recognizerClient.analyzeVoice(multipartFile);
 
         Authorized authorized = (Authorized) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         UserDto user = userClient.getUser(authorized.getId()); // Пользователь точно будет, ибо иначе авторизация не пустит сюда
--- bff/src/main/java/com/gymteam/backend/bff/service/interfaces/VoiceService.java
+++ bff/src/main/java/com/gymteam/backend/bff/service/interfaces/VoiceService.java
@@ -6,9 +6,12 @@ import com.gymteam.backend.bff.exception.VoiceNotMatchingException;
 import com.gymteam.backend.bff.exception.VoiceNotRegisteredException;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+
 public interface VoiceService {
 
     VoiceCommandResponse authorizeVoiceCommand(MultipartFile multipart) throws VoiceNotMatchingException, UnknownVoiceCommandException;
 
-    void registerUserVoice(MultipartFile multipartFile) throws VoiceNotRegisteredException;
+    void registerUserVoice(MultipartFile multipartFile) throws VoiceNotRegisteredException, MalformedURLException, ProtocolException;
 }
